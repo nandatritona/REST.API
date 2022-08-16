6b09df9319b6250e74998f5cbffd15082690f79c
nandatritona <nandhatritona@gmail.com>
2022-08-16 20:58:43 +0700

    memperbaiki path

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from "express"
+import bodyParser from "body-parser"
 
-const app = express();
-const PORT = 5000;
+const app = express()
+const PORT = 5000
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => res.send("Hello From Homepage."))
 
 app.listen(PORT, () =>
     console.log(`Server Running on port: http://127.0.0.1:${PORT}`)
-);
+)
