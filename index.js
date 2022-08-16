d01342184291449eb613037a7a1c66a7d5f0419c
nandatritona <nandhatritona@gmail.com>
2022-08-16 21:10:31 +0700

    Memperbaiki path

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express"
 import bodyParser from "body-parser"
+import usersRoutes from "./routes/users.js"
 
 const app = express()
 const PORT = 5000
 
 app.use(bodyParser.json())
 
-app.get("/", (req, res) => res.send("Hello From Homepage."))
+app.use('/users', usersRoutes)
+
+app.get("/", (req, res) => res.send("API NODE JS AND EXPRESS"))
 
 app.listen(PORT, () =>
     console.log(`Server Running on port: http://127.0.0.1:${PORT}`)
