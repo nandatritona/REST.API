ea49085fa1e619469c22aaf7263a8fde4d2d9527
nandatritona <nandhatritona@gmail.com>
2022-08-17 09:21:28 +0700

    Coba pakage uuid dari npm

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,23 @@
 import express from 'express'
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router()
+const users = []
 
 // all routes in here are starting with /users
 router.get("/", (req, res) => {
-    res.send(`Hallo`);
-});
+    console.log(users);
+    res.send(users);
+})
+
+router.post('/', (req, res) => {
+    const user = req.body;
+    // const userId = uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+    // const userWithId = { ...user, id: uuidv4() }
+
+    users.push({ ...user, id: uuidv4() });
+
+    res.send(`Users with the name ${user.firstName} added to the database!`);
+})
 
 export default router
